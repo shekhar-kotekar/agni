d1d217061ccb6703711a65169244020a1a925578
Chandrashekhar <shekhar.kotekar@gmail.com>
2024-01-16 00:33:29 +0100

    maturin inited

--- rust/src/settings.rs
+++
@@ -1,9 +0,0 @@
-use serde_derive::Deserialize;
-
-#[derive(Debug, Deserialize)]
-pub(crate) struct Aws {
-    pub region: String,
-    pub credentials_file: String,
-    pub bucket_name: String,
-    pub object_key: String,
-}
